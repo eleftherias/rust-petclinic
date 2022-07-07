dff0fa5123f06b73574c336c6559ca2a199b0507
Eleftheria Stein <estein@vmware.com>
2022-07-07 13:02:26 +0200

    Add all owners and pets

--- server/migration/src/m20220101_000010_populate_owners.rs
+++ server/migration/src/m20220101_000010_populate_owners.rs
@@ -39,6 +39,96 @@ impl MigrationTrait for Migration {
         .insert(db)
         .await?;
 
+
+        owner::ActiveModel {
+            first_name: Set("Eduardo".to_owned()), 
+            last_name: Set("Rodriquez".to_owned()), 
+            address: Set("2693 Commerce St.".to_owned()),
+            city: Set("McFarland".to_owned()),
+            telephone: Set("6085558763".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Harold".to_owned()), 
+            last_name: Set("Davis".to_owned()), 
+            address: Set("563 Friendly St.".to_owned()),
+            city: Set("Windsor".to_owned()),
+            telephone: Set("6085553198".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Peter".to_owned()), 
+            last_name: Set("McTavish".to_owned()), 
+            address: Set("2387 S. Fair Way".to_owned()),
+            city: Set("Madison".to_owned()),
+            telephone: Set("6085552765".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Jean".to_owned()), 
+            last_name: Set("Coleman".to_owned()), 
+            address: Set("105 N. Lake St.".to_owned()),
+            city: Set("Monona".to_owned()),
+             telephone: Set("6085552654".to_owned()),
+             ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Jeff".to_owned()), 
+            last_name: Set("Black".to_owned()), 
+            address: Set("1450 Oak Blvd.".to_owned()),
+            city: Set("Monona".to_owned()),
+            telephone: Set("6085555387".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Maria".to_owned()), 
+            last_name: Set("Escobito".to_owned()), 
+            address: Set("345 Maple St.".to_owned()),
+            city: Set("Madison".to_owned()),
+            telephone: Set("6085557683".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("David".to_owned()), 
+            last_name: Set("Schroeder".to_owned()), 
+            address: Set("2749 Blackhawk Trail".to_owned()),
+            city: Set("Madison".to_owned()),
+            telephone: Set("6085559435".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        owner::ActiveModel {
+            first_name: Set("Carlos".to_owned()), 
+            last_name: Set("Estaban".to_owned()), 
+            address: Set("2335 Independence La.".to_owned()),
+            city: Set("Waunakee".to_owned()),
+             telephone: Set("6085555487".to_owned()),
+             ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+
         Ok(())
     }
 }
--- server/migration/src/m20220101_000011_populate_pets.rs
+++ server/migration/src/m20220101_000011_populate_pets.rs
@@ -31,11 +31,157 @@ impl MigrationTrait for Migration {
         .insert(db)
         .await?;
 
+        pet_type::ActiveModel {
+            name: Set("lizard".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet_type::ActiveModel {
+            name: Set("snake".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet_type::ActiveModel {
+            name: Set("bird".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet_type::ActiveModel {
+            name: Set("hamster".to_owned()),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
         pet::ActiveModel {
             name: Set("Leo".to_owned()),
             birth_date: Set(Date::parse_from_str("2000-09-07", "%Y-%m-%d").unwrap()),
             type_id: Set(1),
             owner_id: Set(Some(1)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Basil".to_owned()),
+            birth_date: Set(Date::parse_from_str("2012-08-06", "%Y-%m-%d").unwrap()),
+            type_id: Set(6),
+            owner_id: Set(Some(2)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Rosy".to_owned()),
+            birth_date: Set(Date::parse_from_str("2011-04-17", "%Y-%m-%d").unwrap()),
+            type_id: Set(2),
+            owner_id: Set(Some(3)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Jewel".to_owned()),
+            birth_date: Set(Date::parse_from_str("2010-03-07", "%Y-%m-%d").unwrap()),
+            type_id: Set(2),
+            owner_id: Set(Some(3)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Iggy".to_owned()),
+            birth_date: Set(Date::parse_from_str("2010-11-30", "%Y-%m-%d").unwrap()),
+            type_id: Set(3),
+            owner_id: Set(Some(4)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("George".to_owned()),
+            birth_date: Set(Date::parse_from_str("2010-01-20", "%Y-%m-%d").unwrap()),
+            type_id: Set(4),
+            owner_id: Set(Some(5)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Samantha".to_owned()),
+            birth_date: Set(Date::parse_from_str("2012-09-04", "%Y-%m-%d").unwrap()),
+            type_id: Set(1),
+            owner_id: Set(Some(6)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Max".to_owned()),
+            birth_date: Set(Date::parse_from_str("2012-09-04", "%Y-%m-%d").unwrap()),
+            type_id: Set(1),
+            owner_id: Set(Some(6)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Lucky".to_owned()),
+            birth_date: Set(Date::parse_from_str("2011-08-06", "%Y-%m-%d").unwrap()),
+            type_id: Set(5),
+            owner_id: Set(Some(7)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Mulligan".to_owned()),
+            birth_date: Set(Date::parse_from_str("2007-02-24", "%Y-%m-%d").unwrap()),
+            type_id: Set(2),
+            owner_id: Set(Some(8)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+        pet::ActiveModel {
+            name: Set("Freddy".to_owned()),
+            birth_date: Set(Date::parse_from_str("2010-03-09", "%Y-%m-%d").unwrap()),
+            type_id: Set(5),
+            owner_id: Set(Some(9)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+        pet::ActiveModel {
+            name: Set("Lucky".to_owned()),
+            birth_date: Set(Date::parse_from_str("2010-06-24", "%Y-%m-%d").unwrap()),
+            type_id: Set(2),
+            owner_id: Set(Some(10)),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        pet::ActiveModel {
+            name: Set("Sly".to_owned()),
+            birth_date: Set(Date::parse_from_str("2012-06-08", "%Y-%m-%d").unwrap()),
+            type_id: Set(1),
+            owner_id: Set(Some(10)),
             ..Default::default()
         }
         .insert(db)
